23aab65438a7d7d79fdc583406af38afd3a469df
SvitlanaVelychko <svel2608@gmail.com>
2022-08-12 13:21:54 +0300

    Solve task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+import { GlobalStyle } from "./GlobalStyle";
+import { Box } from "./Box";
+import { Formik } from "formik";
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <Box as="main">
+        <GlobalStyle />
+        <h1>Phonebook</h1>
+        <Formik>
+          <form>
+            <label>Name
+              <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+              />
+            </label>
+            <button type="submit">Add contact</button>
+          </form>
+        </Formik>
+        <Box as="section">
+          <h2>Contacts</h2>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </Box>
+      </Box>
+    );
+  }
 };
